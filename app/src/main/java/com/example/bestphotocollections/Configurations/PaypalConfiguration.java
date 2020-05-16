e7440a2f0f2d4c86ddaf8893820b0467753592b9
Bhavesh Sharma <bhavesh3005sharma@gmail.com>
2020-05-16 12:53:21 +0530

    Paypel payment gateway introduced for sandbox account

---
+++ app/src/main/java/com/example/bestphotocollections/Configurations/PaypalConfiguration.java
@@ -0,0 +1,5 @@
+package com.example.bestphotocollections.Configurations;
+
+public class PaypalConfiguration {
+    public static final String PAYPAL_CLIENT_ID = "AbsNfiJo6Erwr90zEdcvu8VGntuyDnEqJSyztYQdfSJgpvQ1ahyPpOZ6RSGv1a0FMPpsaN57mxwGZEJw";
+}
--- app/src/main/java/com/example/bestphotocollections/showSelectedImg.java
+++ app/src/main/java/com/example/bestphotocollections/showSelectedImg.java
@@ -3,6 +3,7 @@ package com.example.bestphotocollections;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
@@ -17,6 +18,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.bestphotocollections.Configurations.PaypalConfiguration;
 import com.example.bestphotocollections.Model.ItemData;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -26,10 +28,18 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.StorageTask;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
 import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
 
 import java.io.File;
 import java.lang.reflect.Type;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Calendar;
 
@@ -64,6 +74,17 @@ public class showSelectedImg extends AppCompatActivity {
     String str_title;
     String str_metadata;
     ArrayList<ItemData> list;
+    public static final int PAYPAL_REQUEST_CODE = 123;
+    String paymentAmount;
+
+
+    //Paypal Configuration Object
+    private static PayPalConfiguration config = new PayPalConfiguration()
+            // Start with mock environment.  When ready, switch to sandbox (ENVIRONMENT_SANDBOX)
+            // or live (ENVIRONMENT_PRODUCTION)
+            .environment(PayPalConfiguration.ENVIRONMENT_NO_NETWORK)
+            .clientId(PaypalConfiguration.PAYPAL_CLIENT_ID)
+            .merchantName("Photop(Best Photos Collection)");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -87,13 +108,14 @@ public class showSelectedImg extends AppCompatActivity {
 
     @Override
     protected void onDestroy() {
-        super.onDestroy();
+        stopService(new Intent(this, PayPalService.class));
         unbinder.unbind();
+        super.onDestroy();
     }
 
     @OnClick(R.id.download)
     public void download(View view){
-        downloadFile();
+        getPayment();
     }
 
     private void downloadFile() {
@@ -148,5 +170,67 @@ public class showSelectedImg extends AppCompatActivity {
             list = new ArrayList<>();
 
         Log.d("selectedImgLoad",list.toString());
+    }
+
+    private void getPayment() {
+        //Getting the amount from editText
+        paymentAmount =   "1";//price.getText().toString();
+
+        Intent intent_service = new Intent(this, PayPalService.class);
+        intent_service.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+        startService(intent_service);
+
+        //Creating a paypalpayment
+        PayPalPayment payment = new PayPalPayment(new BigDecimal(String.valueOf(paymentAmount)), "USD", "Download Charge ",
+                PayPalPayment.PAYMENT_INTENT_SALE);
+
+        //Creating Paypal Payment activity intent
+        Intent intent = new Intent(this, PaymentActivity.class);
+
+        //putting the paypal configuration to the intent
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+
+        //Puting paypal payment to the intent
+        intent.putExtra(PaymentActivity.EXTRA_PAYMENT, payment);
+
+        //Starting the intent activity for result
+        //the request code will be used on the method onActivityResult
+        startActivityForResult(intent, PAYPAL_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //If the result is from paypal
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PAYPAL_REQUEST_CODE) {
+
+            //If the result is OK i.e. user has not canceled the payment
+            if (resultCode == Activity.RESULT_OK) {
+                //Getting the payment confirmation
+                PaymentConfirmation confirm = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+
+                //if confirmation is not null
+                if (confirm != null) {
+                    try {
+                        //Getting the payment details
+                        String paymentDetails = confirm.toJSONObject().toString(4);
+                        Log.i("paymentExample", paymentDetails);
+
+                        //Starting a new activity for the payment details and also putting the payment details with intent
+                        startActivity(new Intent(this, ConformationActivity.class)
+                                .putExtra("PaymentDetails", paymentDetails)
+                                .putExtra("PaymentAmount", paymentAmount));
+                        downloadFile();
+
+                    } catch (JSONException e) {
+                        Log.e("paymentExample", "an extremely unlikely failure occurred: ", e);
+                    }
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                Log.i("paymentExample", "The user canceled.");
+            } else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID) {
+                Log.i("paymentExample", "An invalid Payment or PayPalConfiguration was submitted. Please see the docs.");
+            }
+        }
     }
 }
